2fb1d1d8378fd91f73d45d008b444527d44e9a14
BarryLiu <1025587160@qq.com>
2016-01-30 21:56:15 +0800

    主要图片的绘制与下面预选的图片的加载

--- FaceBud/src/io/barryliu/facebud/MainActivity.java
+++ FaceBud/src/io/barryliu/facebud/MainActivity.java
@@ -1,19 +1,24 @@
 package io.barryliu.facebud;
 
+import io.barryliu.facebud.itemui.FacePageAdapter;
+import io.barryliu.facebud.lisenter.MyItemLisenter;
 import io.barryliu.facebud.res.MyRes;
 import io.barryliu.facebud.res.MyView;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.app.Activity;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
-import android.view.Menu;
-import android.view.View;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
+import android.view.animation.TranslateAnimation;
 import android.widget.HorizontalScrollView;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RadioGroup.OnCheckedChangeListener;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends FragmentActivity  implements MyItemLisenter{
 
 	public static boolean isBoy = true;
 	private RelativeLayout rl;
@@ -22,6 +27,9 @@ public class MainActivity extends FragmentActivity {
 	private RadioButton rb0;
 	private ViewPager vp;
 	private TextView line;
+	private int fromX;
+	private RadioGroup rg;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -40,14 +48,104 @@ public class MainActivity extends FragmentActivity {
 		rb0 = (RadioButton) findViewById(R.id.rb0);
 		vp = (ViewPager) findViewById(R.id.vp);
 		line = (TextView) findViewById(R.id.line);
+		vp.setAdapter(new FacePageAdapter(getSupportFragmentManager(), this));
+		vp.setOnPageChangeListener(new OnPageChangeListener() {
+			
+			@Override
+			public void onPageSelected(int arg0) {
+				
+			}
+			@Override
+			public void onPageScrolled(int arg0, float arg1, int arg2) {
+				int total = (int)((arg0+arg1)*rb0.getWidth());
+				int green = (vp.getWidth()-rb0.getWidth())/2;
+				int scoll = total - green;
+				hsv.scrollTo(scoll, 0);
+				
+				tvMoveTo(arg0,arg1);
+			}
+			
+			
+			@Override
+			public void onPageScrollStateChanged(int arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
 		
+	}
+	private void tvMoveTo(int index, float f) {
+		RadioButton button = (RadioButton) rg.getChildAt(index);
 		
+		int location [] = new int[2];
+		button.getLocationInWindow(location);
+		
+		TranslateAnimation animation  = new TranslateAnimation(fromX, location[0] + f*button.getWidth(),0,0);
+		animation.setDuration(50);
+		
+		animation.setFillAfter(true);
+		
+		fromX = (int)(location[0]+f*button.getWidth());
+		
+		line.startAnimation(animation);
 		
 	}
 	private void initRadioButton() {
-		// TODO Auto-generated method stub
-		
+		rg = (RadioGroup) findViewById(R.id.rg);
+		rg.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+			
+			@Override
+			public void onCheckedChanged(RadioGroup group, int checkedId) {
+				switch (checkedId) {
+				case R.id.rb0:
+					vp.setCurrentItem(0);
+					break;
+				case R.id.rb1:
+					vp.setCurrentItem(1);
+					break;
+				case R.id.rb2:
+					vp.setCurrentItem(2);
+					break;
+				case R.id.rb3:
+					vp.setCurrentItem(3);
+					break;
+				case R.id.rb4:
+					vp.setCurrentItem(4);
+					break;
+				case R.id.rb5:
+					vp.setCurrentItem(5);
+					break;
+				case R.id.rb6:
+					vp.setCurrentItem(6);
+					break;
+				case R.id.rb7:
+					vp.setCurrentItem(7);
+					break;
+				case R.id.rb8:
+					vp.setCurrentItem(8);
+					break;
+				case R.id.rb9:
+					vp.setCurrentItem(9);
+					break;
+				case R.id.rb10:
+					vp.setCurrentItem(10);
+					break;
+				case R.id.rb11:
+					vp.setCurrentItem(11);
+					break;
+				default:
+					break;
+				}
+			}
+		});
+	}
+	@Override
+	public void myItemClick(int res, int index) {
+		Bitmap bitmap = BitmapFactory.decodeResource(getResources(), res);
+		myView.bms[index] = bitmap;
+		myView.res[index] = res;
+		myView.invalidate();
 	}
 
-
+	
 }
--- FaceBud/src/io/barryliu/facebud/itemui/FaceFragment.java
+++ FaceBud/src/io/barryliu/facebud/itemui/FaceFragment.java
@@ -1,31 +1,157 @@
 package io.barryliu.facebud.itemui;
 
+import io.barryliu.facebud.R;
 import io.barryliu.facebud.lisenter.MyItemLisenter;
+import io.barryliu.facebud.res.MyRes;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
 
-public class FaceFragment extends Fragment{
-	int []res;
-	int type ;
-	boolean gender ;
-	
+public class FaceFragment extends Fragment {
+	int[] res;
+	int type;
+	boolean gender;
+
 	MyItemLisenter onMyItemLisenter;
+
 	public MyItemLisenter getOnMyItemLisenter() {
 		return onMyItemLisenter;
 	}
+
 	public void setOnMyItemLisenter(MyItemLisenter onMyItemLisenter) {
 		this.onMyItemLisenter = onMyItemLisenter;
 	}
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
-		return super.onCreateView(inflater, container, savedInstanceState);
-	}
+		Bundle bundle = getArguments();
+		type = bundle.getInt(MyRes.TAG_TYPE);
+		gender = bundle.getBoolean(MyRes.TAG_ISBOY);
 
+		switch (type) {
+		case 0:
+			if (gender) {
+				// boy 的发现图片地址集合
+				res = MyRes.getBoyHair();
+			} else {
+				res = MyRes.getGirlHair();
+			}
+			break;
+		case 1: 
+			res = MyRes.getFaceShape();
+			break;
+		case 2:
+			res = MyRes.getEyeBrow();
+			break;
+		case 3:
+			res = MyRes.getEye();
+			break;
+		case 4:
+			res = MyRes.getMouth();
+			break;
+		case 5:
+			if(gender){
+				res = MyRes.getFeature();
+			}else{
+				res = MyRes.getGirlFeature();
+			}
+			break;
+		case 6:
+			res = MyRes.getGlass();
+			break;
+		case 7:
+			if(gender){
+				res = MyRes.getBoyClothes();
+			}else{
+				res = MyRes.getGirlClothes();
+			}
+			break;
+		case 8:
+			res = MyRes.getHat();
+			break;
+		case 9:
+			res = MyRes.getHobby();
+			break;
+		case 10:
+			res = MyRes.getBackGround();
+			break;
+		case 11:
+			res = MyRes.getPop();
+			break;
+		}
+
+		
+		GridView gridView =new GridView(getActivity());
+		gridView.setNumColumns(3);
+		gridView.setAdapter(new MyAdapter());
+		gridView.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				int b_res;
+				if(type == 0){
+					if(gender){
+						b_res = MyRes.getRealBoyHair()[position];
+					}else {
+						b_res = MyRes.getRealGirlHair()[position];
+					}
+				}else if(type ==5){
+					if(gender){
+						b_res = MyRes.getRealBoyFeature()[position];
+					}else{
+						b_res = MyRes.getRealGirlFeature()[position];
+					}
+				}else {
+					b_res = res[position];
+				}
+				if(b_res == R.drawable.show_no){
+					b_res = R.drawable.show_nothing;
+				}
+				
+				onMyItemLisenter.myItemClick(b_res, type);
+			}
+		});
+		
+		return gridView;
+	}
 	
-	
+	private class MyAdapter extends BaseAdapter{
+		@Override
+		public int getCount() {
+			return res.length;
+		}
+		@Override
+		public Object getItem(int position) {
+			return res[position];
+		}
+
+		@Override
+		public long getItemId(int position) {
+			return position;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			ImageView imageView =new ImageView(getActivity());
+			imageView.setMinimumWidth(120);
+			imageView.setMinimumHeight(120);
+			
+			imageView.setBackgroundResource(R.drawable.image_bg);
+			
+			imageView.setImageResource(res[position]);
+			return imageView;
+		}
+		
+		
+	}
 }
